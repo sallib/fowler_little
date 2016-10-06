40db68b05ab987b985b87d09af1872e4377cfe8c
sandy <sandy.allibert@gmail.com>
2016-10-06 15:57:54 -0400

    Parse fichier csv

--- GMT4150lab2/src/Main.java
+++ GMT4150lab2/src/Main.java
@@ -1,10 +1,40 @@
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
-	//Main de test
-	public static void main(String[] args) {
-		
+	private final static String FILE_NAME = "Grille_Lab2.csv";
+	private final static int NB_ROW = 7;
+	private final static int NB_COL = 7;
 
+	public static Point[][] createGrid(List<Point> points) {
+		Point[][] grid = new Point[NB_COL][NB_ROW];
+		int index = 0; // index de points
+
+		for (int j = 0; j < NB_COL; j++) { // Parcours des colonnes de
+											// gauche à droite
+			for (int i = NB_ROW - 1; i >= 0; i--) {// Parcours des lignes de bas
+													// en haut
+				grid[i][j] = points.get(index);
+				index++;
+			}
+		}
+		return grid;
+	}
+
+	// Main de test
+	public static void main(String[] args) throws IOException {
+
+		List<Point> list = CsvFileReader.readFile(FILE_NAME);
+
+		Point[][] grid = createGrid(list);
+		for (int i = 0; i < NB_ROW; i++) {
+			for (int j = 0; j < NB_COL; j++) {
+				Point p = grid[i][j];
+				System.out.print(p.getId() + " ");
+			}
+			System.out.println("\n");
+		}
 	}
 
 }
--- GMT4150lab2/src/Point.java
+++ GMT4150lab2/src/Point.java
@@ -1,15 +1,25 @@
 
 public class Point {
 
+	private final int id;
 	private final double x;
 	private final double y;
 	private final double z;
 	private final Type type;
-	
-	public Point(double x, double y, double z){
+
+	public Point(int id, double x, double y, double z) {
+		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.type = Type.INDEFINI;
+	}
+
+	public int getId(){
+		return this.id;
+	}
+	@Override
+	public String toString() {
+		return "Point [id=" + id + "x=" + x + ", y=" + y + ", z=" + z + ", type=" + type + "]";
 	}
 }
