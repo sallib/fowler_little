eba5d911f2d67555c3c9f56a8bfdd680e0395dd0
sandy <sandy.allibert@gmail.com>
2016-10-06 09:29:28 -0400

    Magnifique classe point

---
+++ GMT4150lab2/src/Point.java
@@ -0,0 +1,13 @@
+
+public class Point {
+
+	private final double x;
+	private final double y;
+	private final double z;
+	
+	public Point(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
